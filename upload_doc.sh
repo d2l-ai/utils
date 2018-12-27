3be4e2fe75a0625fe6ffee4f2fa99809e6ca3a24
mli <muli@cs.cmu.edu>
2018-12-27 18:57:27 +0000

    add upload_doc.sh

---
+++ upload_doc.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Upload a doc folder into a S3 bucket, with text context compressed
+#
+# Sample Usage:
+#
+# ./upload_doc.sh build/_build/html/ s3://en.d2l.ai
+#
+# Requres awscli is installed
+
+set -e
+
+if [ $# -ne 2 ]; then
+    echo "ERROR: needs two arguments. "
+    echo "Usage:"
+    echo "  $0 doc_dir s3_bucket"
+    exit -1
+fi
+
+DIR=$1
+BUCKET=$2
+echo "Upload $DIR to $BUCKET"
+
+# use a temp workspace, because we need to modify (compress) some files later.
+rm -rf ${DIR}_tmp
+cp ${DIR} ${DIR}_tmp
+DIR=${DIR}_tmp
+
+
+find $DIR \( -iname '*.css' -o -iname '*.js' \) -exec gzip -9 -n {} \; -exec mv {}.gz {} \;
+
+aws s3 sync --exclude '*.*' --include '*.css' \
+     --content-type 'text/css' \
+     --content-encoding 'gzip' \
+     --acl 'public-read' \
+     $DIR $BUCKET
+
+aws s3 sync --exclude '*.*' --include '*.js' \
+     --content-type 'application/javascript' \
+     --content-encoding 'gzip' \
+     --acl 'public-read' \
+     $DIR $BUCKET
+
+# use a large expire time for fonts
+aws s3 sync --exclude '*.*' --include '*.woff' --include '*.woff2' \
+     --expires "$(date -d '+24 months' --utc +'%Y-%m-%dT%H:%M:%SZ')" \
+     --acl 'public-read' --quiet \
+     $DIR $BUCKET
+
+aws s3 sync --delete $DIR $BUCKET --acl 'public-read' --quiet
