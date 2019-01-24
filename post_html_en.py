688874aa9336d4abb54baab6ba027b40c2a75bb4
Aston Zhang <asv325@gmail.com>
2019-01-24 22:41:03 +0000

    fix post htl

--- post_html_en.py
+++ post_html_en.py
@@ -3,6 +3,6 @@ import subprocess
 DIR = 'build/_build/html'
 
 # Replace with space for all htmls
-p = subprocess.Popen(['bash test.sh replace_with_space ' + DIR], shell=True, executable='/bin/bash')
+p = subprocess.Popen(['bash post_html_utils.sh replace_with_space ' + DIR], shell=True, executable='/bin/bash')
 p.wait()
 
