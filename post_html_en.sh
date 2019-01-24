ed18bd0b2ae384c4373937fa87ce6428ab3f4e68
Aston Zhang <asv325@gmail.com>
2019-01-24 07:40:25 +0000

    add post tex zh

--- post_html_en.sh
+++ post_html_en.sh
@@ -6,5 +6,5 @@ for f in $DIR/chapter*/*html; do
 	sed -i s/Ⓐ/\ /g $f
 done
 
-sed -i s/\<title\>Dive\ into\ Deep\ Learning\ \&\#8212\;\ Dive\ into\ Deep\ Learning\ \ documentation\<\\\/title\>/\<title\>Dive\ into\ Deep\ Learning\ \\\&\#8212\;\ An\ Interactive\ Deep\ Learning\ Book\<\\\/title\>/g $DIR/index.html
+sed -i s/\<title\>Dive\ into\ Deep\ Learning\ \&\#8212\;\ Dive\ into\ Deep\ Learning\ \ documentation\<\\\/title\>/\<title\>Dive\ into\ Deep\ Learning\ \\\&\#8212\;\ An\ Interactive\ Book\ on\ Deep\ Learning\<\\\/title\>/g $DIR/index.html
 
