08c178f57b1f6f333a39a6173d322ca011b86370
Aston Zhang <asv325@gmail.com>
2019-01-24 06:19:24 +0000

    fix post latex

--- post_latex_en.py
+++ post_latex_en.py
@@ -10,7 +10,8 @@ def unnumber_sections(source_file):
     ch2_reached = False
     # Preface and Using this Book are numbered chapters
     NUM_UNNUMBERED_CHAPS = 2
-    TOC2_START_CHAP = '\\chapter{A Taste of Deep Learning}'
+    # A Taste of Deep Learning
+    TOC2_START_CHAP_NO = 4
 
     UNNUMBERED = {'\\section{Summary}',
                   '\\section{References}',
@@ -24,20 +25,21 @@ def unnumber_sections(source_file):
     num_chaps = 0
     with open(target_file, 'w') as target_f, open(source_file, 'r') as source_f:
         for l in source_f:
-            # Unnumber unnumbered chapters
             if l.startswith('\\chapter{'):
                 num_chaps += 1
+                # Unnumber unnumbered chapters
                 if num_chaps <= NUM_UNNUMBERED_CHAPS:
                     chap_name = re.split('{|}', l)[1]
-                    out_line = '\\chapter*{' + chap_name + '}\\addcontentsline{toc}{chapter}{' + chap_name + '}'
-
-            # Set tocdepth to 2 after Chap 1
-            if l.rstrip() == TOC2_START_CHAP:
-                target_f.write('\\addtocontents{toc}{\\protect\\setcounter{tocdepth}{2}}\n')
-                target_f.write(l)
-
+                    out_line = '\\chapter*{' + chap_name + '}\\addcontentsline{toc}{chapter}{' + chap_name + '}\n'
+                    target_f.write(out_line)
+                # Set tocdepth to 2 after Chap 1
+                elif num_chaps == TOC2_START_CHAP_NO:
+                    target_f.write('\\addtocontents{toc}{\\protect\\setcounter{tocdepth}{2}}\n')
+                    target_f.write(l)
+                else:
+                    target_f.write(l)
             # Unnumber all sections in unnumbered chapters
-            if 1 <= num_chaps <= NUM_UNNUMBERED_CHAPS:
+            elif 1 <= num_chaps <= NUM_UNNUMBERED_CHAPS:
                 if l.startswith('\\section'):
                     target_f.write(l.replace('\\section', '\\section*'))
                 elif l.startswith('\\subsection'):
@@ -47,11 +49,10 @@ def unnumber_sections(source_file):
                 else:
                     target_f.write(l)
             # Unnumber summary, references, problems, qr code in numbered chapters
+            elif l.rstrip() in UNNUMBERED:
+                target_f.write(l.replace('section{', 'section*{'))
             else:
-                if l.rstrip() in UNNUMBERED:
-                    target_f.write(l.replace('section{', 'section*{'))
-                else:
-                    target_f.write(l)
+                target_f.write(l)
 
     remove(source_file)
     move(target_file, source_file)
