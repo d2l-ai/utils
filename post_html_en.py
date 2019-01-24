13e14bc5cafa0aec8a480dd2873f0cc909320030
Aston Zhang <asv325@gmail.com>
2019-01-24 22:36:07 +0000

    fix

--- post_html_en.py
+++ post_html_en.py
@@ -1,8 +1,8 @@
 import subprocess
 
-DIR = 'build/_build/html/'
+DIR = 'build/_build/html'
 
 # Replace with space for all htmls
-p = subprocess.Popen(['bash test.sh replace_with_space' + DIR], shell=True, executable='/bin/bash')
+p = subprocess.Popen(['bash test.sh replace_with_space ' + DIR], shell=True, executable='/bin/bash')
 p.wait()
 
