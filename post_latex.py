a3e15c42d9d085e3f105b074e0a953f3d69b5da7
Aston Zhang <asv325@gmail.com>
2019-01-25 07:13:59 +0000

    certer graphics, author table

--- post_latex.py
+++ post_latex.py
@@ -4,6 +4,8 @@ import shutil
 import sys
 import tempfile
 
+MANUAL_STY_FILE = 'build/_build/latex/sphinxmanual.cls'
+
 TEX_FILE_EN = 'build/_build/latex/d2l-en.tex'
 TEX_FILE_ZH = 'build/_build/latex/d2l-zh.tex'
 
@@ -39,7 +41,7 @@ TOC2_START_CHAP_NO_EN = 4
 TOC2_START_CHAP_NO_ZH = 4
 
 
-class TempFile():
+class _TempFile():
     def __init__(self, source_file):
         self.source_file = source_file
         _, self.target_file = tempfile.mkstemp()
@@ -51,12 +53,17 @@ class TempFile():
         os.remove(self.source_file)
         shutil.move(self.target_file, self.source_file)
 
+# E.g., tag = 'begin{figure}'
+def _tag_in_line(tag, line):
+    assert '\\' not in set(tag)
+    return any([elem.startswith(tag) for elem in line.split('\\')])
+
 
 def unnumber_sections(source_file, unnumbered, num_unnumbered_chaps, toc2_start_chap_no):
     preface_reached = False
     ch2_reached = False
     num_chaps = 0
-    with TempFile(source_file) as target_file:
+    with _TempFile(source_file) as target_file:
         with open(target_file, 'w') as target_f, open(source_file, 'r') as source_f:
             for l in source_f:
                 if l.startswith('\\chapter{'):
@@ -89,6 +96,59 @@ def unnumber_sections(source_file, unnumbered, num_unnumbered_chaps, toc2_start_
                     target_f.write(l)
 
 
+def edit_author_style(source_file):
+    with _TempFile(source_file) as target_file:
+        with open(target_file, 'w') as target_f, open(source_file, 'r') as source_f:
+            for l in source_f:
+                if l.lstrip().rstrip() == '\\begin{tabular}[t]{c}':
+                    target_f.write(l.replace('{c}', '{r}'))
+                else:
+                    target_f.write(l)
+
+
+def center_graphics(source_file):
+    with _TempFile(source_file) as target_file:
+        tabulary_cnt = 0
+        figure_cnt = 0
+
+        with open(target_file, 'w') as target_f, open(source_file, 'r') as source_f:
+            for l in source_f:
+                if _tag_in_line('begin{tabulary}', l):
+                    tabulary_cnt += 1
+                elif _tag_in_line('end{tabulary}', l):
+                    tabulary_cnt -= 1
+                elif _tag_in_line('begin{figure}', l):
+                    figure_cnt += 1
+                elif _tag_in_line('end{figure}', l):
+                    figure_cnt -= 1
+
+                if tabulary_cnt == 0 and figure_cnt == 0 and _tag_in_line('sphinxincludegraphics', l):
+                    outs = []
+                    for token in l.split('\\'):
+                        SIG = 'sphinxincludegraphics'
+                        if token.startswith(SIG):
+                            with_centers = ['\\begin{center}']
+                            rest_s = token[len(SIG):]
+                            parenthesis_cnt = 0
+                            for i, s in enumerate(rest_s):
+                                if s == '{':
+                                    parenthesis_cnt += 1
+                                elif s == '}':
+                                    parenthesis_cnt -= 1
+                                if parenthesis_cnt == 0:
+                                    break
+                            with_centers.append('\\sphinxincludegraphics' + rest_s[:i+1])
+                            with_centers.append('\\end{center}')
+                            with_centers.append(rest_s[i+1:])
+                            target_f.write(''.join(with_centers))
+                        else:
+                            outs.append(token)
+                    target_f.write('\\'.join(outs))
+
+                else:
+                    target_f.write(l)
+
+
 if __name__ == '__main__':
     assert len(sys.argv) == 2
     lang = sys.argv[1]
@@ -106,4 +166,6 @@ if __name__ == '__main__':
         toc2_start_chap_no = TOC2_START_CHAP_NO_ZH
 
     unnumber_sections(tex_file, unnumbered, num_unnumbered_chaps, toc2_start_chap_no)
+    edit_author_style(MANUAL_STY_FILE)
+    center_graphics(tex_file)
 
