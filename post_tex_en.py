7cda35e57e1a352ede42e6087b28e436108acf03
Aston Zhang <asv325@gmail.com>
2019-01-25 02:01:56 +0000

    refactor post tex en using with

--- post_tex_en.py
+++ post_tex_en.py
@@ -4,8 +4,20 @@ import shutil
 import tempfile
 
 
+class TempFile():
+    def __init__(self, source_file):
+        self.source_file = source_file
+        _, self.target_file = tempfile.mkstemp()
+
+    def __enter__(self):
+        return self.target_file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        os.remove(self.source_file)
+        shutil.move(self.target_file, self.source_file)
+
+
 def unnumber_sections(source_file):
-    _, target_file = tempfile.mkstemp()
     preface_reached = False
     ch2_reached = False
     # Preface and Using this Book are numbered chapters
@@ -27,40 +39,39 @@ def unnumber_sections(source_file):
                   '\\subsection{Scan the QR Code to Discuss}'}
 
     num_chaps = 0
-    with open(target_file, 'w') as target_f, open(source_file, 'r') as source_f:
-        for l in source_f:
-            if l.startswith('\\chapter{'):
-                num_chaps += 1
-                # Unnumber unnumbered chapters
-                if num_chaps <= NUM_UNNUMBERED_CHAPS:
-                    chap_name = re.split('{|}', l)[1]
-                    out_line = '\\chapter*{' + chap_name + '}\\addcontentsline{toc}{chapter}{' + chap_name + '}\n'
-                    target_f.write(out_line)
-                # Set tocdepth to 2 after Chap 1
-                elif num_chaps == TOC2_START_CHAP_NO:
-                    target_f.write('\\addtocontents{toc}{\\protect\\setcounter{tocdepth}{2}}\n')
-                    target_f.write(l)
+    with TempFile(source_file) as target_file:
+        with open(target_file, 'w') as target_f, open(source_file, 'r') as source_f:
+            for l in source_f:
+                if l.startswith('\\chapter{'):
+                    num_chaps += 1
+                    # Unnumber unnumbered chapters
+                    if num_chaps <= NUM_UNNUMBERED_CHAPS:
+                        chap_name = re.split('{|}', l)[1]
+                        out_line = '\\chapter*{' + chap_name + '}\\addcontentsline{toc}{chapter}{' + chap_name + '}\n'
+                        target_f.write(out_line)
+                    # Set tocdepth to 2 after Chap 1
+                    elif num_chaps == TOC2_START_CHAP_NO:
+                        target_f.write('\\addtocontents{toc}{\\protect\\setcounter{tocdepth}{2}}\n')
+                        target_f.write(l)
+                    else:
+                        target_f.write(l)
+                # Unnumber all sections in unnumbered chapters
+                elif 1 <= num_chaps <= NUM_UNNUMBERED_CHAPS:
+                    if l.startswith('\\section'):
+                        target_f.write(l.replace('\\section', '\\section*'))
+                    elif l.startswith('\\subsection'):
+                        target_f.write(l.replace('\\subsection', '\\subsection*'))
+                    elif l.startswith('\\subsubsection'):
+                        target_f.write(l.replace('\\subsubsection', '\\subsubsection*'))
+                    else:
+                        target_f.write(l)
+                # Unnumber summary, references, problems, qr code in numbered chapters
+                elif l.rstrip() in UNNUMBERED:
+                    target_f.write(l.replace('section{', 'section*{'))
                 else:
                     target_f.write(l)
-            # Unnumber all sections in unnumbered chapters
-            elif 1 <= num_chaps <= NUM_UNNUMBERED_CHAPS:
-                if l.startswith('\\section'):
-                    target_f.write(l.replace('\\section', '\\section*'))
-                elif l.startswith('\\subsection'):
-                    target_f.write(l.replace('\\subsection', '\\subsection*'))
-                elif l.startswith('\\subsubsection'):
-                    target_f.write(l.replace('\\subsubsection', '\\subsubsection*'))
-                else:
-                    target_f.write(l)
-            # Unnumber summary, references, problems, qr code in numbered chapters
-            elif l.rstrip() in UNNUMBERED:
-                target_f.write(l.replace('section{', 'section*{'))
-            else:
-                target_f.write(l)
-
-    os.remove(source_file)
-    shutil.move(target_file, source_file)
 
 
 tex_file = 'build/_build/latex/d2l-en.tex'
+
 unnumber_sections(tex_file)
