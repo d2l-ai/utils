e43cb9a6180dfcbf9fc3a210e0984f645cd6f558
Aston Zhang <astonz@amazon.com>
2019-07-04 02:37:05 -0700

    fix

--- run_ipynb.sh
+++ run_ipynb.sh
@@ -2,15 +2,15 @@
 set -ex
 
 if [ $# -ne 1 ]; then
-	echo "Usage: $0 PATH_TO_NOTEBOOKS"
+	echo "Usage: bash $0 NOTEBOOKS"
+	echo "E.g., bash run_notebooks.sh 'chap/*'"
 	echo "Execute all the notebooks and save outputs (assuming with Python 3)."
 	exit -1
 fi
 
-PATH_TO_NOTEBOOKS=$1
+NOTEBOOKS=$1
 
-for f in $PATH_TO_NOTEBOOKS; do
+for f in $NOTEBOOKS; do
 	echo "==Executing $f"
 	jupyter nbconvert --execute --ExecutePreprocessor.kernel_name=python3 --to notebook --inplace $f
 done
-
