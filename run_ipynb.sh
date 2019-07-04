6db5b49f77a4bd8cf410bd3200faeaaf7b3cea6d
Aston Zhang <astonz@amazon.com>
2019-07-04 02:34:01 -0700

    tab

--- run_ipynb.sh
+++ run_ipynb.sh
@@ -2,9 +2,9 @@
 set -ex
 
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 PATH_TO_NOTEBOOKS"
-    echo "Execute all the notebooks and save outputs (assuming with Python 3)."
-    exit -1
+	echo "Usage: $0 PATH_TO_NOTEBOOKS"
+	echo "Execute all the notebooks and save outputs (assuming with Python 3)."
+	exit -1
 fi
 
 PATH_TO_NOTEBOOKS=$1
