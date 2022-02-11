8488049b46731c43b57fdb36e0bfefb3e5d99cdf
Anirudh Dagar <anirudhdagar6@gmail.com>
2022-02-12 00:00:04 +0100

    Fix: Update the font links and improve script (#2)
    * Fix: Update the font links and improve script
    
    This fixes all the broken links of the fonts
    
    * Add Inconsolata font
    
    * add fc-cache install comment

--- install_fonts.sh
+++ install_fonts.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-# Install fonts to build PDf
+# Install fonts to build PDF
+
+
+###########################################################
+# Make sure system font dir exists already, if not, run:
+# sudo mkdir /usr/share/fonts/opentype/
+
+# Make sure unzip is installed already, if not, run:
+# sudo apt-get install unzip
+
+# fc-cache is required, if not already installed, run:
+# sudo apt install fontconfig
+###########################################################
+
 
 # En
 
@@ -15,44 +28,44 @@ wget -O source-code-pro.zip https://www.fontsquirrel.com/fonts/download/source-c
 unzip source-code-pro -d source-code-pro
 sudo mv source-code-pro /usr/share/fonts/opentype/
 
+wget -O Inconsolata.zip https://www.fontsquirrel.com/fonts/download/Inconsolata
+unzip Inconsolata -d Inconsolata
+sudo mv Inconsolata /usr/share/fonts/opentype/
+
 sudo fc-cache -f -v
 
 # Zh
-wget https://github.com/adobe-fonts/source-han-sans/raw/release/OTF/SourceHanSansSC.zip
-wget https://github.com/adobe-fonts/source-han-serif/raw/release/OTF/SourceHanSerifSC_SB-H.zip
-wget https://github.com/adobe-fonts/source-han-serif/raw/release/OTF/SourceHanSerifSC_EL-M.zip
+wget https://github.com/adobe-fonts/source-han-sans/releases/download/2.004R/SourceHanSansSC.zip
+wget -O SourceHanSerifSC.zip https://github.com/adobe-fonts/source-han-serif/releases/download/2.001R/09_SourceHanSerifSC.zip
 
-unzip SourceHanSansSC.zip
-unzip SourceHanSerifSC_EL-M.zip
-unzip SourceHanSerifSC_SB-H.zip
+unzip SourceHanSansSC.zip -d SourceHanSansSC
+unzip SourceHanSerifSC.zip -d SourceHanSerifSC
 
-sudo mv SourceHanSansSC SourceHanSerifSC_EL-M SourceHanSerifSC_SB-H /usr/share/fonts/opentype/
+sudo mv SourceHanSansSC SourceHanSerifSC /usr/share/fonts/opentype/
 sudo fc-cache -f -v
 
 # KO
 
-wget https://github.com/adobe-fonts/source-han-sans/raw/release/OTF/SourceHanSansK.zip
-wget https://github.com/adobe-fonts/source-han-serif/raw/release/OTF/SourceHanSerifK_SB-H.zip
-wget https://github.com/adobe-fonts/source-han-serif/raw/release/OTF/SourceHanSerifK_EL-M.zip
+wget https://github.com/adobe-fonts/source-han-sans/releases/download/2.004R/SourceHanSansK.zip
+wget -O SourceHanSerifK.zip https://github.com/adobe-fonts/source-han-serif/releases/download/2.001R/08_SourceHanSerifK.zip
 
-unzip SourceHanSansK.zip
-unzip SourceHanSerifK_EL-M.zip
-unzip SourceHanSerifK_SB-H.zip
+unzip SourceHanSansK.zip -d SourceHanSansK
+unzip SourceHanSerifK.zip -d SourceHanSerifK
 
-sudo mv SourceHanSansK SourceHanSerifK_EL-M SourceHanSerifK_SB-H /usr/share/fonts/opentype/
+sudo mv SourceHanSansK SourceHanSerifK /usr/share/fonts/opentype/
 sudo fc-cache -f -v
 
 # JA
 
-wget https://github.com/adobe-fonts/source-han-sans/raw/release/OTF/SourceHanSansJ.zip
-wget https://github.com/adobe-fonts/source-han-serif/raw/release/OTF/SourceHanSerifJ_SB-H.zip
-wget https://github.com/adobe-fonts/source-han-serif/raw/release/OTF/SourceHanSerifJ_EL-M.zip
+wget https://github.com/adobe-fonts/source-han-sans/releases/download/2.004R/SourceHanSansJ.zip
+wget -O SourceHanSerifJ.zip https://github.com/adobe-fonts/source-han-serif/releases/download/2.001R/07_SourceHanSerifJ.zip
 
-unzip SourceHanSansJ.zip
-unzip SourceHanSerifJ_EL-M.zip
-unzip SourceHanSerifJ_SB-H.zip
+unzip SourceHanSansJ.zip -d SourceHanSansJ
+unzip SourceHanSerifJ.zip -d SourceHanSerifJ
 
-sudo mv SourceHanSansJ SourceHanSerifJ_EL-M SourceHanSerifJ_SB-H /usr/share/fonts/opentype/
+sudo mv SourceHanSansJ SourceHanSerifJ /usr/share/fonts/opentype/
 sudo fc-cache -f -v
 
-rm source*.zip
+
+# Remove all zip files
+rm Source*.zip source*.zip Inconsolata.zip
